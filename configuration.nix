a5bf476f1fdcd2c96b2729ef583a4fd00073ad5d
Guy <savsguy115@gmail.com>
2026-03-09 00:14:25 -0400

    changed neovim configuration

--- configuration.nix
+++ configuration.nix
@@ -786,8 +786,32 @@ in
         -- here your custom Lua configuration goes!
         vim.opt.number = true
         vim.opt.swapfile = false
+        vim.opt.backup = false
+        vim.opt.undofile = true
+        vim.opt.tabstop = 4
+        vim.opt.softtabstop = 4
+        vim.opt.shiftwidth = 4
+        vim.opt.expandtab = true
+        vim.opt.autoindent = true
         vim.opt.smartindent = true
         vim.opt.syntax = "on"
+        vim.opt.incsearch = true
+        vim.opt.inccommand = "split"
+        vim.opt.ignorecase = true
+        vim.opt.smartcase = true
+        vim.opt.termguicolors = true
+        vim.opt.background = "dark"
+        vim.opt.scrolloff = 8
+        vim.opt.signcolumn = "yes"
+        vim.opt.backspace = {"start", "eol", "indent"}
+        vim.opt.splitright = true
+        vim.opt.plitbelow = true
+        vim.opt.updatetime = 50
+        vim.opt.colorcolumn = "80"
+        vim.opt.clipboard:append("unnamedplus")
+        vim.opt.hlsearch = true
+        vim.opt.mouse = "a"
+        vim.g.editorconfig = true
       '';
     };
   };
